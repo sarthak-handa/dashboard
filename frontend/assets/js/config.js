5b510619636cf907ffaec48045ab5650bffee76c
sarthak-handa <sarthakh23@gmail.com>
2026-08-22 13:41:50 +0530

    trial

--- frontend/assets/js/config.js
+++
@@ -1,7 +0,0 @@
-// const API_URL = "https://ydprojects.ydagchmi.com/api";
-// const API_URL = "http://localhost:5000"; //FOR TESTING ON LOCAL SERVER
-const API_URL = "https://37503xtx-5000.inc1.devtunnels.ms"; //FOR TESTING ON LOCAL SERVER
-// const API_URL = "https://s9zg3g8p-5000.inc1.devtunnels.ms"; //FOR TESTING ON LOCAL SERVER
- 
-// WEBHOOK LOCAL URL = "https://s9zg3g8p-5000.inc1.devtunnels.ms/webhooks/whatsapp";
-// WEBHOOK LIVE URL = "https://ydprojects.ydagchmi.com/api/webhooks/whatsapp";
